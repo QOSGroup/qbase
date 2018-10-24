6050c39bc07be5068772e769bb9036ab08c865cf
wangzhiyong <wangzhiyong@tokenxy.cn>
2018-10-24 13:22:37 +0800

    modify qcp result tag

--- qcp/qcp.go
+++ qcp/qcp.go
@@ -1,8 +1,8 @@
 package qcp
 
 const (
-	QcpFrom      = "qcp.From"
-	QcpTo        = "qcp.To"
-	QcpSequence  = "qcp.Sequence"
-	QcpHashBytes = "qcp.hashbytes"
+	QcpFrom     = "qcp.from"
+	QcpTo       = "qcp.to"
+	QcpSequence = "qcp.sequence"
+	QcpHash     = "qcp.hash"
 )
