c73095f7d32b7996b063e55d2ab4131dec3c8e3f
imuge <imuge@qq.com>
2018-11-08 16:45:40 +0800

    修改创世账户公私钥保存、显示方式

--- example/basecoin/cmd/basecli/main.go
+++ example/basecoin/cmd/basecli/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	bcli "github.com/QOSGroup/qbase/client"
 	"github.com/QOSGroup/qbase/client/account"
+	"github.com/QOSGroup/qbase/client/keys"
 	"github.com/QOSGroup/qbase/example/basecoin/app"
 	"github.com/QOSGroup/qbase/example/basecoin/tx/client"
+	"github.com/QOSGroup/qbase/example/basecoin/types"
 	"github.com/QOSGroup/qbase/version"
 	"github.com/spf13/cobra"
 	"github.com/tendermint/tendermint/libs/cli"
-	"os"
 )
 
 var (
@@ -25,20 +26,26 @@ func main() {
 
 	rootCmd.AddCommand(bcli.LineBreak)
 
+	// account
 	rootCmd.AddCommand(
 		bcli.GetCommands(account.QueryAccountCmd(cdc))...)
-
 	rootCmd.AddCommand(bcli.LineBreak)
 
+	// keys
+	rootCmd.AddCommand(
+		bcli.GetCommands(keys.Commands(cdc))...)
+	rootCmd.AddCommand(bcli.LineBreak)
+
+	// basecoin
 	client.AddCommands(rootCmd, cdc)
-
 	rootCmd.AddCommand(bcli.LineBreak)
 
+	// version
 	rootCmd.AddCommand(
 		version.VersionCmd,
 	)
 
-	executor := cli.PrepareMainCmd(rootCmd, "BC", os.ExpandEnv("$HOME/.basecli"))
+	executor := cli.PrepareMainCmd(rootCmd, "BC", types.DefaultCLIHome)
 	err := executor.Execute()
 	if err != nil {
 		panic(err)
