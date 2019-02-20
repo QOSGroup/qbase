80ad7f6210218b45687faec37b441d8154e6da52
wangzhiyong <wangzhiyong@tokenxy.cn>
2019-02-20 11:06:35 +0800

    consensus mapper add comment

--- consensus/codec.go
+++ consensus/codec.go
@@ -5,6 +5,7 @@ import (
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
+//共识参数编码
 func RegisterCodec(cdc *go_amino.Codec) {
 	cdc.RegisterConcrete(&abci.ConsensusParams{}, "abci/consensus/ConsensusParams", nil)
 }
