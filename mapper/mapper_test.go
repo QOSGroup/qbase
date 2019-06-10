777ce9c899d6eb1d54cbe2b2e73da5e31ddd7a50
wangzhiyong <wangzhiyong@tokenxy.cn>
2019-06-10 14:36:30 +0800

    tendermint 版本升级至0.31.5

--- mapper/mapper_test.go
+++ mapper/mapper_test.go
@@ -11,7 +11,7 @@ import (
 	go_amino "github.com/tendermint/go-amino"
 	"github.com/tendermint/tendermint/crypto"
 	"github.com/tendermint/tendermint/crypto/ed25519"
-	"github.com/tendermint/tendermint/crypto/encoding/amino"
+	cryptoAmino "github.com/tendermint/tendermint/crypto/encoding/amino"
 	dbm "github.com/tendermint/tendermint/libs/db"
 )
 
@@ -207,7 +207,12 @@ func TestBaseMapper_IteratorWithType(t *testing.T) {
 func TestDiggggggggggggggggggggHole(t *testing.T) {
 	baseMapper := getMapper()
 
-	prefix := []byte("a")
+	//golang: v1.11
+	// prefix := []byte("a")
+
+	//golang: v.1.12
+	prefix := make([]byte, 0, 8)
+	prefix = append(prefix, byte(97))
 
 	for i := 0; i < 10; i++ {
 		s := strconv.Itoa(i)
