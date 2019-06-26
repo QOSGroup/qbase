d876bfc9b4057cb59b18f3e661fdaaecb6f71bac
wangzhiyong <wangzhiyong@tokenxy.cn>
2019-06-26 15:07:18 +0800

    fix 193: qcp.Hash tag return hex string

--- qcp/qcp.go
+++ qcp/qcp.go
@@ -1,8 +1,20 @@
 package qcp
 
+import (
+	"encoding/hex"
+
+	"github.com/QOSGroup/qbase/txs"
+	"github.com/tendermint/tendermint/crypto"
+)
+
 const (
 	QcpFrom     = "qcp.from"
 	QcpTo       = "qcp.to"
 	QcpSequence = "qcp.sequence"
 	QcpHash     = "qcp.hash"
 )
+
+func GenQcpTxHash(tx *txs.TxQcp) string {
+	bz := crypto.Sha256(tx.BuildSignatureBytes())
+	return hex.EncodeToString(bz)
+}
