9f6fd07ea5a74a84e9b625a7399da1ccad5c2fa3
imuge <imuge@qq.com>
2019-07-05 18:25:45 +0800

    update txs search

--- client/block/txs.go
+++ client/block/txs.go
@@ -3,7 +3,9 @@ package block
 import (
 	"encoding/hex"
 	"errors"
+	"fmt"
 	"strings"
+	"time"
 
 	"github.com/QOSGroup/qbase/client/context"
 	"github.com/QOSGroup/qbase/client/types"
@@ -13,10 +15,11 @@ import (
 	go_amino "github.com/tendermint/go-amino"
 	abci "github.com/tendermint/tendermint/abci/types"
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
+	tmtypes "github.com/tendermint/tendermint/types"
 )
 
 const (
-	flagTags  = "tag"
+	flagTags  = "tags"
 	flagPage  = "page"
 	flagLimit = "limit"
 )
@@ -24,20 +27,47 @@ const (
 func searchTxCmd(cdc *go_amino.Codec) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "txs",
-		Short: "Search for all transactions that match the given tags.",
+		Short: "Query for paginated transactions that match a set of tags",
 		Long: strings.TrimSpace(`
-Search for all transactions that match the given tags.
+Search for transactions that match the exact given tags where results are paginated.
 
-		example:
-$ basecli txs --tag "key1='value1',key2='value2'"
+Example:
+$ <appcli> query txs --tags '<tag1>:<value1>&<tag2>:<value2>' --page 1 --limit 30
 `),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			tags := viper.GetStringSlice(flagTags)
+			tagsStr := viper.GetString(flagTags)
+			tagsStr = strings.Trim(tagsStr, "'")
+
+			var tags []string
+			if strings.Contains(tagsStr, "&") {
+				tags = strings.Split(tagsStr, "&")
+			} else {
+				tags = append(tags, tagsStr)
+			}
+
+			var tmTags []string
+			for _, tag := range tags {
+				if !strings.Contains(tag, ":") {
+					return fmt.Errorf("%s should be of the format <key>:<value>", tagsStr)
+				} else if strings.Count(tag, ":") > 1 {
+					return fmt.Errorf("%s should only contain one <key>:<value> pair", tagsStr)
+				}
+
+				keyValue := strings.Split(tag, ":")
+				if keyValue[0] == tmtypes.TxHeightKey {
+					tag = fmt.Sprintf("%s=%s", keyValue[0], keyValue[1])
+				} else {
+					tag = fmt.Sprintf("%s='%s'", keyValue[0], keyValue[1])
+				}
+
+				tmTags = append(tmTags, tag)
+			}
+
 			page := viper.GetInt(flagPage)
 			limit := viper.GetInt(flagLimit)
 
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
-			txs, err := searchTxs(cliCtx, tags, page, limit)
+			txs, err := QueryTxsByEvents(cliCtx, tmTags, page, limit)
 			if err != nil {
 				return err
 			}
@@ -48,31 +78,35 @@ $ basecli txs --tag "key1='value1',key2='value2'"
 
 	cmd.Flags().StringP(types.FlagNode, "n", "tcp://localhost:26657", "Node to connect to")
 	viper.BindPFlag(types.FlagNode, cmd.Flags().Lookup(types.FlagNode))
-	cmd.Flags().String(types.FlagChainID, "", "Chain ID of Tendermint node")
-	viper.BindPFlag(types.FlagChainID, cmd.Flags().Lookup(types.FlagChainID))
 	cmd.Flags().Bool(types.FlagTrustNode, false, "Trust connected full node (don't verify proofs for responses)")
 	viper.BindPFlag(types.FlagTrustNode, cmd.Flags().Lookup(types.FlagTrustNode))
-	cmd.Flags().StringSlice(flagTags, nil, "Comma-separated list of tags that must match")
-	cmd.Flags().Int(flagPage, 1, "search page")
-	cmd.Flags().Int(flagLimit, 100, "per page limit for result")
+
+	cmd.Flags().String(flagTags, "", "tag:value list of tags that must match")
+	cmd.Flags().Uint32(flagPage, 1, "Query a specific page of paginated results")
+	cmd.Flags().Uint32(flagLimit, 100, "Query number of transactions results per page returned")
+	cmd.MarkFlagRequired(flagTags)
+
 	cmd.Flags().Bool(types.FlagJSONIndet, false, "print indent result json")
 
-	cmd.MarkFlagRequired(flagTags)
 	return cmd
 }
 
 func queryTxCmd(cdc *go_amino.Codec) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "tx [hash]",
-		Short: "Query match hash tx in all commit block",
+		Short: "Query for a transaction by hash in a committed block",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			hashHexStr := args[0]
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
-			output, err := queryTx(cliCtx, hashHexStr)
+			output, err := QueryTx(cliCtx, args[0])
 			if err != nil {
 				return err
 			}
+
+			if output.Empty() {
+				return fmt.Errorf("No transaction found with hash %s", args[0])
+			}
+
 			return cliCtx.PrintResult(output)
 		},
 	}
@@ -94,72 +128,139 @@ type Result struct {
 	Result abci.ResponseDeliverTx `json:"result"`
 }
 
-func formatTxResult(cdc *go_amino.Codec, res *ctypes.ResultTx) (Result, error) {
-
-	var tx btypes.Tx
-	err := cdc.UnmarshalBinaryBare(res.Tx, &tx)
-
-	if err != nil {
-		return Result{}, err
+// QueryTxsByEvents performs a search for transactions for a given set of events
+// via the Tendermint RPC. An event takes the form of:
+// "{eventAttribute}.{attributeKey} = '{attributeValue}'". Each event is
+// concatenated with an 'AND' operand. It returns a slice of Info object
+// containing txs and metadata. An error is returned if the query fails.
+func QueryTxsByEvents(cliCtx context.CLIContext, events []string, page, limit int) (*btypes.SearchTxsResult, error) {
+	if len(events) == 0 {
+		return nil, errors.New("must declare at least one event to search")
 	}
 
-	return Result{
-		Hash:   hex.EncodeToString(res.Hash),
-		Height: res.Height,
-		Tx:     tx,
-		Result: res.TxResult,
-	}, nil
-}
-
-func searchTxs(cliCtx context.CLIContext, tags []string, page, limit int) ([]Result, error) {
-	if len(tags) == 0 {
-		return nil, errors.New("tags is empty")
+	if page <= 0 {
+		return nil, errors.New("page must greater than 0")
 	}
 
-	query := strings.Join(tags, " AND ")
+	if limit <= 0 {
+		return nil, errors.New("limit must greater than 0")
+	}
+
+	// XXX: implement ANY
+	query := strings.Join(events, " AND ")
+
 	node, err := cliCtx.GetNode()
 	if err != nil {
 		return nil, err
 	}
 
 	prove := !cliCtx.TrustNode
-	res, err := node.TxSearch(query, prove, page, limit)
+
+	resTxs, err := node.TxSearch(query, prove, page, limit)
 	if err != nil {
 		return nil, err
 	}
-	//todo 校验tx
 
-	results := make([]Result, len(res.Txs))
-	for i, k := range res.Txs {
-		results[i], err = formatTxResult(cliCtx.Codec, k)
+	// prove
+
+	resBlocks, err := getBlocksForTxResults(cliCtx, resTxs.Txs)
+	if err != nil {
+		return nil, err
+	}
+
+	txs, err := formatTxResults(cliCtx.Codec, resTxs.Txs, resBlocks)
+	if err != nil {
+		return nil, err
+	}
+
+	result := btypes.NewSearchTxsResult(resTxs.TotalCount, len(txs), page, limit, txs)
+
+	return &result, nil
+}
+
+// QueryTx queries for a single transaction by a hash string in hex format. An
+// error is returned if the transaction does not exist or cannot be queried.
+func QueryTx(cliCtx context.CLIContext, hashHexStr string) (btypes.TxResponse, error) {
+	hash, err := hex.DecodeString(hashHexStr)
+	if err != nil {
+		return btypes.TxResponse{}, err
+	}
+
+	node, err := cliCtx.GetNode()
+	if err != nil {
+		return btypes.TxResponse{}, err
+	}
+
+	resTx, err := node.Tx(hash, !cliCtx.TrustNode)
+	if err != nil {
+		return btypes.TxResponse{}, err
+	}
+
+	resBlocks, err := getBlocksForTxResults(cliCtx, []*ctypes.ResultTx{resTx})
+	if err != nil {
+		return btypes.TxResponse{}, err
+	}
+
+	out, err := formatTxResult(cliCtx.Codec, resTx, resBlocks[resTx.Height])
+	if err != nil {
+		return out, err
+	}
+
+	return out, nil
+}
+
+// formatTxResults parses the indexed txs into a slice of TxResponse objects.
+func formatTxResults(cdc *go_amino.Codec, resTxs []*ctypes.ResultTx, resBlocks map[int64]*ctypes.ResultBlock) ([]btypes.TxResponse, error) {
+	var err error
+	out := make([]btypes.TxResponse, len(resTxs))
+	for i := range resTxs {
+		out[i], err = formatTxResult(cdc, resTxs[i], resBlocks[resTxs[i].Height])
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	return results, nil
+	return out, nil
 }
 
-func queryTx(cliCtx context.CLIContext, hashHexStr string) (Result, error) {
-	hash, err := hex.DecodeString(hashHexStr)
-	if err != nil {
-		return Result{}, err
-	}
-
+func getBlocksForTxResults(cliCtx context.CLIContext, resTxs []*ctypes.ResultTx) (map[int64]*ctypes.ResultBlock, error) {
 	node, err := cliCtx.GetNode()
 	if err != nil {
-		return Result{}, err
+		return nil, err
 	}
 
-	res, err := node.Tx(hash, cliCtx.TrustNode)
+	resBlocks := make(map[int64]*ctypes.ResultBlock)
+
+	for _, resTx := range resTxs {
+		if _, ok := resBlocks[resTx.Height]; !ok {
+			resBlock, err := node.Block(&resTx.Height)
+			if err != nil {
+				return nil, err
+			}
+
+			resBlocks[resTx.Height] = resBlock
+		}
+	}
+
+	return resBlocks, nil
+}
+
+func formatTxResult(cdc *go_amino.Codec, resTx *ctypes.ResultTx, resBlock *ctypes.ResultBlock) (btypes.TxResponse, error) {
+	tx, err := parseTx(cdc, resTx.Tx)
 	if err != nil {
-		return Result{}, err
+		return btypes.TxResponse{}, err
 	}
 
-	result, err := formatTxResult(cliCtx.Codec, res)
+	return btypes.NewResponseResultTx(resTx, tx, resBlock.Block.Time.Format(time.RFC3339)), nil
+}
+
+func parseTx(cdc *go_amino.Codec, txBytes []byte) (btypes.Tx, error) {
+	var tx btypes.Tx
+
+	err := cdc.UnmarshalBinaryBare(txBytes, &tx)
 	if err != nil {
-		return Result{}, err
+		return nil, err
 	}
 
-	return result, nil
+	return tx, nil
 }
